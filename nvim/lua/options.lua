65602edce9c80b84c5014744de45ef9e5d73ab8c
aetizazsameer <asameer@princeton.edu>
2024-02-10 14:47:54 -0500

    Update nvim keyboard shortcuts

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,7 +4,7 @@ vim.api.nvim_exec("language en_US", true)
 vim.opt.shell = "bash"
 vim.opt.shellcmdflag = "-c"
 vim.opt.shellquote = ""
-vim.g.terminal_emulator="bash"
+vim.g.terminal_emulator = "bash"
 -- enable line numbers
 vim.wo.number = true
 -- convert tabs to spaces
@@ -18,21 +18,23 @@ vim.g.maplocalleader = " "
 -- center cursor while scrolling
 vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+-- allow G for g shortcuts
+vim.api.nvim_set_keymap("n", "G", "g", { noremap = true, silent = true })
 -- toggle window shortcut
 vim.keymap.set("n", "<C-n>", "<C-w>w")
 -- enable cursorline in insert mode
 local cursorlinegroup = vim.api.nvim_create_augroup("CursorLineInsert", { clear = true })
 vim.api.nvim_create_autocmd("InsertEnter", {
-	pattern = "*",
-	group = cursorlinegroup,
-	callback = function()
-		vim.opt.cursorline = true
-	end,
+  pattern = "*",
+  group = cursorlinegroup,
+  callback = function()
+    vim.opt.cursorline = true
+  end,
 })
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	group = cursorlinegroup,
-	callback = function()
-		vim.opt.cursorline = false
-	end,
+  pattern = "*",
+  group = cursorlinegroup,
+  callback = function()
+    vim.opt.cursorline = false
+  end,
 })
--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,22 +1,32 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.api.nvim_create_autocmd("VimEnter", {
-			pattern = "*",
-			group = vim.api.nvim_create_augroup("NeotreeOnOpen", { clear = true }),
-			once = true,
-			callback = function()
-				if vim.fn.argc() <= 1 then
-					vim.cmd("Neotree filesystem reveal right")
-					vim.cmd("wincmd w")
-				end
-			end,
-		})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "gn", ":Neotree filesystem reveal right toggle<CR>")
+    -- open neotree at startup
+    vim.api.nvim_create_autocmd("VimEnter", {
+      pattern = "*",
+      group = vim.api.nvim_create_augroup("NeotreeOnOpen", { clear = true }),
+      once = true,
+      callback = function()
+        if vim.fn.argc() <= 1 then
+          vim.cmd("Neotree filesystem reveal right")
+          vim.cmd("wincmd w")
+        end
+      end,
+    })
+    -- open neotree on new tab
+    vim.api.nvim_create_autocmd("TabNew", {
+      group = vim.api.nvim_create_augroup("NeotreeOnNewTab", { clear = true }),
+      callback = function()
+        vim.cmd("Neotree filesystem reveal right")
+        vim.cmd("wincmd w")
+      end,
+    })
+  end,
 }
